2004f90a714e5cb19f20a32b91e8eb0f12ac2ac9
Mosjoandy <34078510+Mosjoandy@users.noreply.github.com>
2018-04-26 22:48:57 -0700

    added pseudo code from notebook and phone image, cleaned up code blocks and spacing

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -27,7 +27,8 @@ console.log(wordArray);
 
 }
 // set guesses left to 15
-// set guessed letters to []
+// set all arrays to empty
+// display random word generated
 var resetGame = function() {
     lives = 15;
     guesses= [];
@@ -36,6 +37,7 @@ var resetGame = function() {
     document.getElementById("currentWord").textContent = wordArray;
 }
 
+// displaying intial lives, wins, and randomized word
     randomizer();
     document.getElementById("showLives").textContent = lives;
     document.getElementById("showWins").textContent = wins;
@@ -43,42 +45,40 @@ var resetGame = function() {
 
 // user presses keys
 // record key presses into guesses[]
-// display guesses on screen
 document.onkeyup = function(letterGuess) {
     guesses.push(letterGuess.key);
     theLetter = letterGuess.key;
     console.log("You pressed: " + guesses);
     document.getElementById("showGuesses").textContent = guesses;
 
-    
+// if lives = 0, reset the game, generate new random word    
 if (lives === 0) {
     resetGame();
     randomizer();
-    
-}
+    }
 
+// for loop to use key pressed (theLetter) to equal wordArray[i]
 else {
     for (var i = 0; i < wordArray.length; i++) {
         if (theLetter === hiddenArray[i]) {
             wordArray[i] = theLetter;
             document.getElementById("currentWord").textContent = wordArray;
         } 
-        
     }
-    
+// if the letter pressed didn't equal any letter in hiddenArray,
+// subtract a live and update live counter
     if (theLetter != hiddenArray[i]) {
         lives = (lives - 1);
         document.getElementById("showLives").textContent = lives;
 
+// if the wordArray becomes equal to hiddenArray
+// add a win to wins counter, reset game, generate new random word
     } else if (wordArray === hiddenArray) {
         wins = (wins + 1);
+        resetGame();
         randomizer();
-        
-    
     }
 }
-
-
 }
 
 // if key press = not letter of word
